f018152938cf720e6a78c2e05ebf6b08e066a67b
Anuar Jiménez <anuar.jimenez@itexico.com>
2019-12-26 10:09:06 -0600

    [RELEASE] Build with hardcoded data

---
+++ build/precache-manifest.a1efbb26f81f7b16ec0a500243ad3078.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14eac8dd4a551140ab697ddffaf2c97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "886efa1073145abeb6d1",
+    "url": "/static/css/main.35b8666f.chunk.css"
+  },
+  {
+    "revision": "81ae782c328d2354d401",
+    "url": "/static/js/2.858f96c6.chunk.js"
+  },
+  {
+    "revision": "886efa1073145abeb6d1",
+    "url": "/static/js/main.907ada68.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
