dea21f24a874e4cf3ba1441fcfc0e564b97b3d6d
Anuar <anuar.jimenez@itexico.com>
2020-08-09 13:13:35 -0500

    Add default styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,6 +14,7 @@ import Footer from "shared/components/Footer";
 
 import darkTheme from "shared/providers/theme/darkTheme";
 import "./normalizeStyles.scss";
+import "./appStyles.scss";
 
 const App: React.FC = (props) => {
   return (
