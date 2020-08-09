9707e602e5f13bb351c79de9d10ce593d2c2e335
Anuar <anuar.jimenez@itexico.com>
2020-08-09 13:20:35 -0500

    Add header component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,13 +4,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "react-jss";
 
 import About from "features/About/About";
+import { Header, Footer } from "app/components/Layout";
 
 /*
 import { ProjectContainer } from "./projects/projects.container";
 import { SkillsContainer } from "./skills/skills.container";
 import { FactsComponent } from "./facts/facts.component";
 */
-import Footer from "shared/components/Footer";
 
 import darkTheme from "shared/providers/theme/darkTheme";
 import "./normalizeStyles.scss";
@@ -20,6 +20,7 @@ const App: React.FC = (props) => {
   return (
     <Router>
       <ThemeProvider theme={darkTheme}>
+        <Header />
         <section className='landing-page'>
           <About />
           {/*
