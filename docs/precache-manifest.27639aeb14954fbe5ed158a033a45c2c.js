6eddb5a1863f6c833e8ade708e10822443f90518
Anuar <anuar.jimenez@itexico.com>
2020-08-19 18:20:31 -0500

    Add build

---
+++ docs/precache-manifest.27639aeb14954fbe5ed158a033a45c2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a035bcfe86c07e14a31abdfe5a15b0bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fef09a3f752089470bbd",
+    "url": "./static/css/main.eb8b2108.chunk.css"
+  },
+  {
+    "revision": "0100b27e205572662624",
+    "url": "./static/js/2.0bea96ee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.0bea96ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef09a3f752089470bbd",
+    "url": "./static/js/main.a4595d1e.chunk.js"
+  },
+  {
+    "revision": "6e8b55f3732c44c198fa",
+    "url": "./static/js/runtime-main.97771229.js"
+  },
+  {
+    "revision": "c05f86bf91897a9e17b3393dff3cc99e",
+    "url": "./static/media/js.c05f86bf.jpg"
+  },
+  {
+    "revision": "41d22b08d745d995729400638deb352c",
+    "url": "./static/media/react.41d22b08.png"
+  }
+]);
